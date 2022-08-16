4619e7fc0e7fd1487f3a79b139f13dcdbc4b56f2
Gabriel Villanueva <gvillanuevavega@gmail.com>
2022-08-16 13:26:49 -0500

    Se termino la parte estadística, se aprendioo a calcular el promedio, mediana y moda. Se utilizaron muchos métodos de arreglos y objetos dentro dde arreglos.

--- estadistica.js
+++ estadistica.js
@@ -2,12 +2,13 @@ const number= document.querySelector('.number')
 const prom= document.querySelector('.prom')
 const guardar= document.querySelector('.guardar')
 const resultado= document.querySelector('.resultado')
-const parImpar= document.querySelector('.parImpar')
+const med= document.querySelector('.medianaP')
+const medButton= document.querySelector('.med')
 
 
 guardar.addEventListener('click',genLista)
 prom.addEventListener('click',promLista)
-
+medButton.addEventListener('click',medLista)
 
 let lista=[]
 function genLista(){
@@ -19,12 +20,13 @@ function genLista(){
         
             if(lista.length%2==0){
                 
-                parImpar.innerText='La cantidad de elementos en la lista es par'
-                
+                let indexListaPar=Math.floor(lista.length / 2 )
+
             }else{
+                let indexListaImpar=Math.floor(lista.length / 2) -1
                 
-                parImpar.innerText="La cantidad de elementos en la lista es impar";
             };
+            return promLista()
         break
         
     }
@@ -35,5 +37,70 @@ function promLista(){
     console.log(`Los elementos de la lista son: ${lista}`)
     promedio=(lista.reduce((a,b) => parseInt(a)+parseInt(b)))/lista.length
     resultado.innerText="El promedio es: " + promedio;
-    return;
+    const ordenarLista= lista.sort((a,b)=> a-b)
+console.log(ordenarLista)
 }
+
+function medLista(lista){
+
+    if(lista.length%2==0){
+        console.log("Soy par")
+        let medInteriorInf=lista.length/2
+        let medInteriorSup=(lista.length/2)+1
+        let medInterior=(medInteriorInf+medInteriorSup)/2
+        console.log(medInterior)
+    }else{
+        console.log("Soy impar")
+    }
+}
+
+const ordenarLista= lista.sort((a,b)=> a-b)
+console.log(ordenarLista)
+
+
+function modaLista(lista){
+    let listaCount={}
+    
+    for(let i=0; i<lista.length;i++){
+        let elemento= lista[i]
+        if(listaCount[elemento]){
+            listaCount[elemento] +=1
+
+        }else{
+            listaCount[elemento]=1
+        }
+    }
+    let listArray= Object.entries(listaCount);
+    console.log({listaCount,listArray})
+}
+lista=[1,2,3,3,2,1,4,4,2,1,5,2,2]
+ordenarListaBidimensional(lista)
+function ordenarListaBidimensional(lista){
+    let listaCount={}
+
+    for(let i=0; i<lista.length ;i++){
+        let elemento=lista[i]
+        if(listaCount[elemento]){
+            listaCount[elemento]+=1;
+        }else{
+            listaCount[elemento]=1;
+        }
+
+    }
+    let ordenarLista= lista.sort((a,b)=> a-b)
+    let listArray=Object.entries(listaCount)
+
+
+  console.log(listArray.sort((a,b)=> a[1]-b[1]))
+  
+
+}
+
+
+function promedioPonderado(){
+
+   let notas=[6,2,3,4,5,8]
+   let prom=((notas.reduce((a,b)=> a+b )- (notas[notas.length-1]))*10+  (notas[notas.length-1]*50))/100
+   console.log(prom)
+}
+promedioPonderado()
